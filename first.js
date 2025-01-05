84527b2cfa3a464bafcde9c74741e5ede3a2ee8d
etsu0217 <loverock29690217@gmail.com>
2025-01-06 00:01:22 +0900

    Add files via upload

---
+++ first.js
@@ -0,0 +1,49 @@
+var variableNumer = 1234;
+console.log(variableNumer);
+
+var variableText = "Text";
+console.log(variableText);
+
+var variableBoolean = true;
+console.log(variableBoolean);
+
+//課題
+var addNumber = 10 + 20;
+console.log(addNumber);
+
+var addNumber = 20 - 10;
+console.log(addNumber);
+
+var addNumber = 50 * 2;
+console.log(addNumber);
+
+var addNumber = 20 / 2;
+console.log(addNumber);
+
+var combinationText = "12" + "34";
+console.log(combinationText);
+
+//if文
+var boolean = true;
+if (boolean) {
+    console.log(boolean);
+};
+
+var num = 100
+if (num == 100) {
+    console.log(num);
+}
+
+//文字列として100を代入
+var num = "100"
+//比較する値は数値型の100
+if (num == 100) {
+    //==で比較しているので文字列でも数値型でもtrueになる。
+    console.log(num);
+}
+
+//課題
+var text = "JavaScript"
+if (text == "JavaScript") {
+    console.log(text);
+}
